57be3daf001d51914c1d7df69d6d127d6ebc2935
marcellushan <mhannah@highlands.edu>
2017-08-24 08:02:06 -0400

    aapi 2

---
+++ app/PreAapi.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PreAapi extends Model
+{
+    protected $fillable = ['cycle_id','expectations','empathy','corporal','roles','power','family_id'];
+
+}
