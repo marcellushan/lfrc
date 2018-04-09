aefa000d68503df627a6cccc5fb45c28eea1dfc8
Marc Hannah <mhannah@highlands.edu>
2018-04-09 05:54:13 -0400

    remove close

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -48,8 +48,13 @@
 
         <div class="row">
             <div class="col-md-3 col-md-offset-3">
-                <h3>Status</h3>
-                Open <input type="radio" name="status" value="1"> Closed <input type="radio" name="status" value="2">
+                <h3>Types of Abuse</h3>
+                <select name="abuse">
+                    <option value="">Select</option>
+                    @foreach($abuses as $abuse)
+                        <option value="{{$abuse->short_name}}">{{$abuse->name}}</option>
+                @endforeach
+                </select>
             </div>
             <div class="col-md-3">
                 <h3>Parent Aide</h3>
@@ -57,66 +62,6 @@
                     <option value="">Select</option>
                     @foreach($parent_aides as $parent_aide)
                         <option value="{{$parent_aide->id}}">{{$parent_aide->last_name}}, {{$parent_aide->first_name}}</option>
-                    @endforeach
-                </select>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3 col-md-offset-3">
-                <h3>Types of Abuse</h3>
-                <select name="abuse">
-                    <option value="">Select</option>
-                    @foreach($abuses as $abuse)
-                        <option value="{{$abuse->short_name}}">{{$abuse->name}}</option>
-                    @endforeach
-                </select>            </div>
-            <div class="col-md-3">
-                <h3>Types of Re-Abuse</h3>
-                <select name="reabuses">
-                    <option value="">Select</option>
-                    @foreach($abuses as $abuse)
-                        <option value="{{$abuse->short_name}}">{{$abuse->name}}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="row">
-                <div class="col-md-3 col-md-offset-5">
-       <button type="submit" class="btn-primary btn-lg">submit</button>
-       </div>
-        </div>
-    </form>
-
-    <h2 align="center">Cases Closed</h2>
-    <form action="{{URL::to('/')}}/report/show" name="report" id="report" method="post">
-        {{ csrf_field() }}
-        <div class="row">
-            <div class="col-md-3 col-md-offset-3">
-                <h3>From Date</h3>
-                @include('partials.date_needed', ['name' => 'start_date'])
-            </div>
-            <div class="col-md-3">
-                <h3>To Date</h3>
-                @include('partials.date_needed', ['name' => 'end_date'])
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-md-3 col-md-offset-3">
-                <h3>Parent Aide</h3>
-                <select name="parent_aide_id">
-                    <option value="">Select</option>
-                    @foreach($parent_aides as $parent_aide)
-                        <option value="{{$parent_aide->id}}">{{$parent_aide->last_name}}, {{$parent_aide->first_name}}</option>
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-md-3">
-                <h3>Types of Abuse</h3>
-                <select name="abuse">
-                    <option value="">Select</option>
-                    @foreach($abuses as $abuse)
-                        <option value="{{$abuse->short_name}}">{{$abuse->name}}</option>
                     @endforeach
                 </select>
             </div>
@@ -142,8 +87,68 @@
             {{--</div>--}}
             <div class="row">
                 <div class="col-md-3 col-md-offset-5">
-                    <button type="submit" class="btn-primary btn-lg">submit</button>
-                </div>
-            </div>
+       <button type="submit" class="btn-primary btn-lg">submit</button>
+       </div>
+        </div>
+    </form>
+
+    {{--<h2 align="center">Cases Closed</h2>--}}
+    {{--<form action="{{URL::to('/')}}/report/show" name="report" id="report" method="post">--}}
+        {{--{{ csrf_field() }}--}}
+        {{--<div class="row">--}}
+            {{--<div class="col-md-3 col-md-offset-3">--}}
+                {{--<h3>From Date</h3>--}}
+                {{--@include('partials.date_needed', ['name' => 'start_date'])--}}
+            {{--</div>--}}
+            {{--<div class="col-md-3">--}}
+                {{--<h3>To Date</h3>--}}
+                {{--@include('partials.date_needed', ['name' => 'end_date'])--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+        {{--<div class="row">--}}
+            {{--<div class="col-md-3 col-md-offset-3">--}}
+                {{--<h3>Parent Aide</h3>--}}
+                {{--<select name="parent_aide_id">--}}
+                    {{--<option value="">Select</option>--}}
+                    {{--@foreach($parent_aides as $parent_aide)--}}
+                        {{--<option value="{{$parent_aide->id}}">{{$parent_aide->last_name}}, {{$parent_aide->first_name}}</option>--}}
+                    {{--@endforeach--}}
+                {{--</select>--}}
+            {{--</div>--}}
+            {{--<div class="col-md-3">--}}
+                {{--<h3>Types of Abuse</h3>--}}
+                {{--<select name="abuse">--}}
+                    {{--<option value="">Select</option>--}}
+                    {{--@foreach($abuses as $abuse)--}}
+                        {{--<option value="{{$abuse->short_name}}">{{$abuse->name}}</option>--}}
+                    {{--@endforeach--}}
+                {{--</select>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+        {{--<div class="row">--}}
+            {{--<div class="col-md-3 col-md-offset-3">--}}
+                {{--<h3>Types of Abuse</h3>--}}
+                {{--<select name="abuse">--}}
+                    {{--<option value="">Select</option>--}}
+                    {{--@foreach($abuses as $abuse)--}}
+                        {{--<option value="{{$abuse->short_name}}">{{$abuse->name}}</option>--}}
+                    {{--@endforeach--}}
+                {{--</select>            </div>--}}
+            {{--<div class="col-md-3">--}}
+                {{--<h3>Types of Re-Abuse</h3>--}}
+                {{--<select name="reabuses">--}}
+                    {{--<option value="">Select</option>--}}
+                    {{--@foreach($abuses as $abuse)--}}
+                        {{--<option value="{{$abuse->short_name}}">{{$abuse->name}}</option>--}}
+                    {{--@endforeach--}}
+                {{--</select>--}}
+            {{--</div>--}}
+            {{--<div class="row">--}}
+                {{--<div class="col-md-3 col-md-offset-5">--}}
+                    {{--<button type="submit" class="btn-primary btn-lg">submit</button>--}}
+                {{--</div>--}}
+            {{--</div>--}}
     </form>
 @endsection
